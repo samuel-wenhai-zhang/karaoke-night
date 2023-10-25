23124ac6ad92d3ce88967b9b7082d4c921f82c27
Boltcrasher <Boltcrasher@gmail.com>
2023-10-25 13:30:44 -0500

    karaoke

--- KaraokeDJ.java
+++ KaraokeDJ.java
@@ -3,7 +3,38 @@ import java.util.LinkedList;
 import java.util.Scanner;
 
 public class KaraokeDJ {
-    
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        Queue<PlayListEntry> playlist = new LinkedList<PlayListEntry>();
 
-
+        outerloop:
+        while (true) {
+            System.out.println(
+                "Would you like to:\n" + 
+                "1. Add a song\n" + 
+                "2. Get the next song in the queue\n" + 
+                "3. Exit"
+            );
+            try {
+                String choice = in.nextLine();
+                switch (choice) {
+                    case "1":
+                        do {
+                            
+                        } while (choice.equals(""));
+                        break;
+                    case "2":
+                        break;
+                    case "3":
+                        break outerloop;
+                    default:
+                        System.out.println("Invalid input.");
+                }
+            }
+            catch (Exception e) {
+                System.out.println("Invalid input.");
+            }
+            
+        }
+    }
 }
--- Song.java
+++ Song.java
@@ -1,7 +1,50 @@
 public class Song {
-    
-    // create the Song class and necessary constructors.
+    private String title;
+    private String artist;
+    private String length;
+    private int index;
 
-    // Be sure to include getters and setters and a toString
+    public Song(String title, String artist, String length, int index) {
+        this.title = title;
+        this.artist = artist;
+        this.length = length;
+        this.index = index;
+    }
 
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public String getLength() {
+        return length;
+    }
+
+    public void setLength(String length) {
+        this.length = length;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    public void setIndex(int index) {
+        this.index = index;
+    }
+
+    @Override
+    public String toString() {
+        return index + " - " + title + " by " + artist + "  " + length;
+    }
 }
